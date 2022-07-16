03079db68a343a021ea7f6bbb75930ec61875196
justinchore <jchoustin91@gmail.com>
2022-07-15 18:38:30 -0700

    Account Info Dictionary

--- account_functions.py
+++ account_functions.py
@@ -6,8 +6,28 @@
 def create_account():
     account_info = {}
     print('First Name: ')
-
+    first_name = input()
+    #validation here
+    #place into account_info
+    account_info[first_name] = first_name
     print('Last Name: ')
+    last_name = input()
+    #validation here
+    #place into account_info
+    account_info[first_name] = first_name
     print('Email Address: ')
+    email = input()
+    #validation here
+    #place into account_info
+    account_info[first_name] = first_name
     print('Initial Balance Deposit: ')
-
+    #validation here
+    #place into account_info
+    account_info[first_name] = first_name
+    balance = input()
+    #validation here
+    #place into account_info
+    account_info[first_name] = first_name
+    #show user information, ask for confirmation
+    #save account! (Here or main?)
+    return account_info
