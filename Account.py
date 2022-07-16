c54c8d0aa3e381c2d00d9785f9a9a0e15ce45d2f
justinchore <jchoustin91@gmail.com>
2022-07-15 18:09:05 -0700

    First Commit

---
+++ Account.py
@@ -0,0 +1,9 @@
+class Account:
+    #Create constructor for attributes:
+    def __init__(self, email, password, balance, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._email = email
+        self._balance = balance
+    
+   
---
+++ Bankyappy.py
@@ -0,0 +1,29 @@
+accounts = {} #Get Data here
+
+def main():
+    run_program = True
+    while run_program:
+        print('Welcome to Foundation Bank!')
+        print('Please select an option from the menu')
+        print('1: Log in to your account')
+        print('2: Create a Foundation Bank account')
+        print('3: Exit')
+
+        user_selection = int(input())
+
+        match user_selection:
+            case 1:
+                print('Log in to your account')
+            case 2:
+                print('Create an account')
+            case 3: 
+                print('exit')
+                run_program = False
+
+
+    return None
+    
+
+if __name__ == "__main__":
+    main()
+        
